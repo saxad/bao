688d4ac2212f5745d601fbfdb1e720a9b75fb15a
saxad <saad_zizi@hotmail.com>
2020-08-03 23:31:03 +0200

    custom login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,7 +41,7 @@
                         
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 ">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -52,12 +52,17 @@
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                        <div class="form-group row ">
+                            <div class="col-md-12 ">
+                                <button type="submit" class="btn btn-block  login-btn">
                                     {{ __('Login') }}
                                 </button>
 
+                            </div>
+                        </div>  
+                        <div class="row">
+                            <div class="col-md-8 offset-md-2 ">
+                                
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
