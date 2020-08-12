0b448afd527b75bfad52a4f6338c736a91144c42
saxad <saad_zizi@hotmail.com>
2020-08-12 22:20:53 +0200

    fillable field in geocutil table

--- app/GeocutilRecording.php
+++ app/GeocutilRecording.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class GeocutilRecording extends Model
 {
-    //
+    protected $fillable = [
+        "sessionid",
+        "no",
+        "versant",
+        "parent",
+        "child",
+        "type",
+        "type_inter",
+        "type_poste",
+        "type_troncon",
+        "nom",
+        "etat",
+        "code_gdo",
+        "depart",
+        "poste_secours",
+        "depart_secours",
+        "code_gdo_depart_secours",
+        "clients_number",
+        "child_number",
+        "puissance"
+    ];
+    
 }
