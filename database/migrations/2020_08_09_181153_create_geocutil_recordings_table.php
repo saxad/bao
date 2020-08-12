81e467a7e3d67f45d4bc9a0d0eb2c80544ebe683
saxad <saad_zizi@hotmail.com>
2020-08-12 14:07:43 +0200

    temporary table schema

--- database/migrations/2020_08_09_181153_create_geocutil_recordings_table.php
+++ database/migrations/2020_08_09_181153_create_geocutil_recordings_table.php
@@ -15,7 +15,26 @@ class CreateGeocutilRecordingsTable extends Migration
     {
         Schema::create('geocutil_recordings', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("sessionid");
+            $table->integer("no");
+            $table->string("versant");
+            $table->string("parent");
+            $table->string("child");
+            $table->string("type");
+            $table->string("type_inter");
+            $table->string("type_poste");
+            $table->string("type_troncon");
+            $table->string("nom");
+            $table->char("etat",1);
+            $table->string("code_gdo");
+            $table->string("depart");
+            $table->string("poste_secours");
+            $table->string("depart_secours");
+            $table->string("code_gdo_depart_secours");
+            $table->string("clients_number");
+            $table->string("child_number");
+            $table->string("puissance");
+            //$table->timestamps();
         });
     }
 
